f179c917de955414139ec4d77d3ba242cba506de
zubairch1199 <133282511+chshahid119@users.noreply.github.com>
2024-03-23 02:06:37 +0500

    added searching and use react-router loader parms

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/searchOrder";
 
 function Header() {
   return (
     <div>
       <Link to="/">Fast React Pizza Co.</Link>
+      <SearchOrder />
     </div>
   );
 }
