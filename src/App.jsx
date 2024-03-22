1902ceb27b38ac1932d194a3f163a9332cf970aa
zubairch1199 <133282511+chshahid119@users.noreply.github.com>
2024-03-23 00:28:32 +0500

    add v6 react routing to pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,32 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./ui/Home";
+import Menu, { loader as menuLoader } from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
+import AppLayout from "./ui/AppLayout";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/menu",
+        element: <Menu />,
+        loader: menuLoader,
+      },
+      { path: "/cart", element: <Cart /> },
+      { path: "/order/new", element: <CreateOrder /> },
+      { path: "/order/:orderId", element: <Order /> },
+    ],
+  },
+]);
 function App() {
-  const x = 23;
-  return <div>Hello vite</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/ui/AppLayout.jsx
@@ -0,0 +1,25 @@
+import Header from "./Header";
+import CartOverview from "../features/cart/CartOverview";
+import { Outlet, useNavigation } from "react-router-dom";
+import Loader from "./Loader";
+
+function AppLayout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
+
+  return (
+    <div className="layout">
+      {isLoading && <Loader />}
+
+      <Header />
+      <main>
+        <h1>Content</h1>
+        <Outlet />
+      </main>
+
+      <CartOverview />
+    </div>
+  );
+}
+
+export default AppLayout;
