6713b0f3dd8990097d85f60ccee41c6ac7efd0ce
zubairch1199 <133282511+chshahid119@users.noreply.github.com>
2024-03-26 06:09:47 +0500

    add new feature of prioritize using useFetcher.form

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import CreateOrder, {
 import Order, { loader as orderLoader } from "./features/order/Order";
 import AppLayout from "./ui/AppLayout";
 import Error from "./ui/Error";
+import { action as updateOrderAction } from "./features/order/UpdateOrder";
 
 const router = createBrowserRouter([
   {
@@ -35,12 +36,12 @@ const router = createBrowserRouter([
         element: <Order />,
         loader: orderLoader,
         errorElement: <Error />,
+        action: updateOrderAction,
       },
     ],
   },
 ]);
 function App() {
-
   return <RouterProvider router={router} />;
 }
 
--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -9,6 +9,7 @@ import {
   formatDate,
 } from "../../utils/helpers";
 import { useEffect } from "react";
+import UpdateOrder from "./UpdateOrder";
 
 function Order() {
   const order = useLoaderData();
@@ -98,11 +99,14 @@ function Order() {
           To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}
         </p>
       </div>
+
+      {!priority && <UpdateOrder order={order} />}
     </div>
   );
 }
 
 export async function loader({ params }) {
+  console.log(params);
   const order = await getOrder(params.orderId);
   return order;
 }
