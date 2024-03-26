ca534e03e9e82d6e5b2fafc944d2c8c1610f2045
zubairch1199 <133282511+chshahid119@users.noreply.github.com>
2024-03-26 05:29:19 +0500

    add ingredients fetching through useFetcher hook

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -1,17 +1,28 @@
 // Test ID: IIDSAT
 
 import OrderItem from "./OrderItem";
-import { useLoaderData } from "react-router-dom";
+import { useFetcher, useLoaderData } from "react-router-dom";
 import { getOrder } from "../../services/apiRestaurant";
 import {
   calcMinutesLeft,
   formatCurrency,
   formatDate,
 } from "../../utils/helpers";
+import { useEffect } from "react";
 
 function Order() {
   const order = useLoaderData();
-  // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
+  const fetcher = useFetcher();
+
+  useEffect(
+    function () {
+      if (!fetcher.data && fetcher.state === "idle") fetcher.load("/menu");
+    },
+    [fetcher],
+  );
+
+  // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude
+  //  names or address, these are only for the restaurant staff
   const {
     id,
     status,
@@ -62,7 +73,15 @@ function Order() {
 
       <ul className="divide-y dive-stone-200 border-b border-t">
         {cart.map((item) => (
-          <OrderItem item={item} key={item.id} />
+          <OrderItem
+            item={item}
+            key={item.pizzaId}
+            isLoadingIngredients={fetcher?.state === "loading"}
+            ingredients={
+              fetcher?.data?.find((el) => el.id === item.pizzaId)
+                ?.ingredients ?? []
+            }
+          />
         ))}
       </ul>
 
