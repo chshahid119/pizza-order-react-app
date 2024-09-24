a0b093cb60640823ae08e8bfd69ae0a5ab2494db
Shahidch19 <chshahidofficial119@gmail.com>
2024-09-24 21:40:58 +0500

    add new updated code

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ import CreateOrder, {
   action as createOrderAction,
 } from './features/order/CreateOrder';
 import Order, { loader as orderLoader } from './features/order/Order';
+import { action as updateOrderAction } from './features/order/UpdateOrder';
+
 import AppLayout from './ui/AppLayout';
 
 const router = createBrowserRouter([
@@ -37,6 +39,7 @@ const router = createBrowserRouter([
         element: <Order />,
         loader: orderLoader,
         errorElement: <Error />,
+        action: updateOrderAction,
       },
     ],
   },
